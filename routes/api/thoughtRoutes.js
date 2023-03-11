778acee0a5654727681113bbc67364fcc82ed851
oliviasylee <oliviasylee@gmail.com>
2023-03-10 19:10:22 -0500

    Add routes for thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,14 +1,29 @@
+// Routes that include functions needed to write a RESTful API for thoughts and reactions
 const router = require('express').Router();
-// /api/thoughts
+const {
+  allThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
 
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
 // GET to get all thoughts
+router.route('/').get(allThoughts).post(createThought);
+
 // GET to get a single thought by its _id
 // POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
 // PUT to update a thought by its _id
 // DELETE to remove a thought by its _id
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 // POST to create a reaction stored in a single thought's reactions array field
 // DELETE to pull and remove a reaction by the reaction's reactionId value
+router.route('/:thoughtId/reactions/:reactionId').post(createReaction).delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,4 @@
+// Routes that include functions needed to write a RESTful API for users and friends
 const router = require('express').Router();
 const {
   getUsers,
@@ -10,7 +11,7 @@ const {
 
 } = require('../../controllers/userController');
 
-// /api/users - router for api/users endpoint
+// /api/users
 // GET all users and POST(Create) a new user
 router.route('/').get(getUsers).post(createUser);
 
@@ -22,8 +23,7 @@ router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 // Bonus - Remove a user's associated thoughts when deleted
 
 // /api/users/:userId/friends/:friendId
-// POST to add a new friend to a user's friend list
-// DELETE to remove a friend from a user's friend list
+// POST to add a new friend to a user's friend list & DELETE to remove a friend from a user's friend list
 router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
 
 module.exports = router;
