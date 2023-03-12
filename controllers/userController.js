155153ad9f2a7706ee6211a8b33f41a70dab6137
oliviasylee <oliviasylee@gmail.com>
2023-03-12 15:52:32 -0400

    Update userController - deleteUser, add a new friend, and remove a friend.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const { User, Thought } = require('../models');
 
 module.exports = {
   // Get all users
@@ -32,9 +32,7 @@ module.exports = {
   updateUser(req,res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { username: req.params.username,
-        email: req.params.email 
-      },
+      { ...req.body }, // Spread operator to decompose JSON-formatted data into key-value pairs
       { runValidators: true, new: true } // Required to enable data validation and return an updated user
       )
       .then((user) =>
@@ -45,17 +43,49 @@ module.exports = {
           .catch((err) => res.status(500).json(err));
   },
 
-  // createFriend(Post) to add a new friend to a user's friend list
-  // deleteFriend(Delete) to remove a friend from a user's friend list
-
-  // Delete a user
-  deleteUser(req, res) {
-    User.findOneAndRemove({ _id: req.params.UserId })
-      .then((user) => 
+  // createFriend(Post) 
+  // Add a new friend to a user's friend list - Find user and then update friends
+  createFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body }},
+      // If you add a friend who is already in the list again, it won't be duplicated.
+      { runValidators: true, new: true },
+      { new: true }
+    )
+        .then((user) =>
         !user
-          ? res.status(404).json({ message: 'No such user exists' })
+          ? res.status(404).json({ message: 'No user with this id!' })
           : res.json(user)
       )
+        .catch((err) => res.status(500).json(err));
+  },
+
+  // deleteFriend(Delete) to remove a friend from a user's friend list
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.body }},
+      { runValidators: true, new: true }
+    )
+      .then((user) => 
+        !user 
+          ? res.status(404).json({ message: 'No user with this id!' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete a user - Remove a user's associated thoughts when deleted.
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'No such user exists' });
+        }
+        return Thought.deleteMany({ username: req.params.userId });
+      })
+      .then(() => res.json({ message: 'User and associated thoughts deleted' }))
       .catch((err) => res.status(500).json(err));
   },
 };
