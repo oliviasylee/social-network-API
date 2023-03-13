aa499562e15cba44ce9b09680f275f8e3411ff27
oliviasylee <oliviasylee@gmail.com>
2023-03-12 22:58:51 -0400

    Add seed data and helper functions

--- utils/data.js
+++ utils/data.js
@@ -1,1 +1,76 @@
-// data.js
+const usernames = [
+  'glowingstarfish',
+  'happypenguin',
+  'coolcat',
+  'funkycheetah',
+  'luckyowl',
+  'sillymonkey',
+  'colorfulparrot',
+  'jollygiraffe',
+  'bravekoala',
+  'bouncykangaroo'
+];
+
+const thoughtsBodies = [
+  'This is a tweet.',
+  'Another tweet.',
+  'RT if you agree!',
+  'I can\'t believe this just happened.',
+  'This is big news.',
+  'New blog post alert!',
+  'Just published a new video!',
+  'I have some exciting news to share!',
+  'I\'m working on a new project.',
+  'Stay tuned for updates!',
+];
+
+const possibleReactions = [
+  'Wow, I never thought of it that way!',
+  'Thank you for sharing this.',
+  'You\'ve given me something to think about.',
+  'Keep up the great work!',
+  'I\'m not sure how I feel about this.', 
+  'This is really thought-provoking.',
+  'I completely agree with you.',
+  'You\'re changing the game!',
+  'I\'m learning so much from your tweets.',
+  'I appreciate your perspective.',
+];
+
+const users = [];
+
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Gets a random username
+const getRandomName = () =>
+  `${getRandomArrItem(usernames)}`;
+
+// Function to generate random thoughts that we can add to the database. Includes reactions.
+const getRandomThoughts = (int) => {
+  let results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughtText: getRandomArrItem(thoughtsBodies),
+      reactions: [...getRandomReactions(3)],
+    });
+  }
+  return results;
+};
+
+// Create the reactions that will be added to each thought
+const getRandomReactions = (int) => {
+  if (int === 1) {
+    return getRandomArrItem(possibleReactions);
+  }
+  let results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      responseBody: getRandomArrItem(possibleReactions),
+      username: getRandomName(),
+    });
+  }
+  return results;
+};
+
+module.exports = { getRandomName, getRandomThoughts, getRandomReactions };
--- utils/seed.js
+++ utils/seed.js
@@ -1,1 +1,31 @@
-// seed.js
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { getRandomName, getRandomThoughts } = require('./data')
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  // Drop existing thoughts and users
+  await Thought.deleteMany({});
+  await User.deleteMany({});
+
+  // Create empty arrayt to hold the users
+  const users = [];
+  const thoughts = getRandomThoughts(10);
+
+  // Loop 10 times -- add username to the username array
+  for (let i = 0; i < 10; i++) {
+    const username = getRandomName();
+    users.push({ username });
+  }
+
+   // Add users and thoughts to the collection and await the results
+  await User.collection.insertMany(users);
+  await Thought.collection.insertMany(thoughts);
+
+  console.table(users);
+  console.table(thoughts);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
